b44dc0e30ce794ac6a91c7af6e3a6078c0b83df6
hala-ahmedd <hm2300277@tkh.edu.eg>
2024-12-08 01:07:58 +0200

    3rd commit
    1. slight changes in the prompt of the input message 2. added comments

--- int only.py
+++ int only.py
@@ -1,76 +1,82 @@
 """"
-This code will be divided into 3 sections: generating the key, encryption and decryption. 
+El Gamal Encryption and Decryption algorithm
+This application will be divided into 3 main sections: generating the key, encryption and decryption. 
+It outputs encrypted and decrypted messages using El Gamal algorithm.
+prime number: any number divided by 1 or itself ONLY
 """
-import random
+import random #a module used in generating a random number during the encryption process
 def is_prime(prime):
-        if prime <= 1: #negatives or 1 aren't a prime number
+        if prime <= 1: #negatives or 1 aren't prime numbers
             return False
         else: 
-            for i in range(2, int(prime**0.5) + 1): #iterates from 2 to the square root+1 of the prime number, the prime number itself is not there
+            for i in range(2, int(prime**0.5) + 1): #iterates from 2 to the square root+1 of the prime number, 1 and the prime number itself are not in range
                 if prime % i == 0: #if divisible by any:
                     return False
             return True #prime number
 
 def generating_keys (prime):
     global x 
-    x= int(input ("Enter private key= "))
+    x= int(input ("Enter private key= ")) #takes the private key as an input
     global g 
-    g=2 #as g should be greater than 1 and smaller than p
-    while g<prime:
+    g=2 #initialization: as g should be greater than 1 and smaller than p
+    while g<prime: #as long as the g is smaller than prime, conditions under the loop will occur
         check=[] #results
-        for i in range(1,prime):
-            result= pow(g,i,prime)
-            check.append (result)
-        if len(check) != len(set(check)): #if repiteted condition
-            g+=1
+        for i in range(1,prime): #i=power, range: 1<g<p-2
+            result= pow(g,i,prime) #g^i % p 
+            check.append (result) #adds the result of the calculation in the "check" list
+        if len(check) != len(set(check)): #checks if there's a repeated result
+            g+=1 #increments the g by 1 and loops again
         else:
             print(f"G is equal to {g}")
-            break
+            break #stops the loop
     global y 
-    y=pow(g, x,prime)
-    return (f"public key and private key: {prime, g, y , x}") 
+    y=pow(g, x,prime) #g^x mod p
+    return (f"Public key and Private key: {prime, g, y , x}") 
 
 def encryption(m, prime, g, y):
-    b = random.randint(2, prime - 2)  # Generate random numbers key b with a range from 2 to prime-2
+    b = random.randint(2, prime - 2)  # generates random number(b) with a range from 2 to prime-2
     global c1
     c1 = pow(g, b, prime)  # c1 = g^b mod p
     global c2
-    c2 = (m * pow(y, b, prime)) % prime  # c2 = m * y^b mod p
-    return (f"The encrypted message is equal to {c1, c2}") 
+    c2 = (m * pow(y, b, prime)) % prime  # c2 = (m * (y^b mod p)) mod p
+    return (f"The encrypted message is equal to {c1, c2}") #returns encrypted message
 
 # Function to decrypt a message using ElGamal
 def decryption(c1, c2, prime, x):
     z = pow(c1, x, prime)  # z = c1^x mod p
-    s = pow(z, prime - 2, prime)  # Modular inverse of z using Fermat's Little Theorem
-    original_message = (c2 * s) % prime  # Decrypted message = c2 * s mod p
-    return (f"The decrypted message is equal to {original_message}")
+    s = pow(z, prime - 2, prime)  # s=z^(prime -2) mod p
+    original_message = (c2 * s) % prime  # decrypted message = c2 * s mod p
+    return (f"The decrypted message is equal to {original_message}") # returns decrypted message
 
 #user's code:
 #user's input:
 message = None #intializing it with nothing just so i could use the variable in a condition
-prime=int(input("enter prime number: "))
+prime=int(input("Enter prime number: "))
+#checking process:
 while not is_prime(prime):  # Looping until a valid prime is entered
     prime = int(input("The number entered is not prime. Please enter a prime number: "))
-while True: #while message has a value of nothing
+while True: #an infinite loop,(will continue until 'broken')
     try:
         message = int(input("Please enter your integer message: "))
-        if message>=prime:
+        if message>=prime: #if the message int value is greater than or equal to prime:
             print("Invalid input, message should be 0<=m<prime number")
         else:
             print(f"You entered: {message}")
-            break  # Exit the loop if input is valid
-    except ValueError:
+            break  # exit the loop when the input is valid
+    except ValueError: #exception handeling: handeling wrong inputs
         print("Invalid input, please enter a number.")
-#main function: the start of every function
+
+#main function: the start of implementation of every function
 def main(message):
-    check=is_prime(prime)
-    while check==False:
+    check=is_prime(prime) # checks if the value 'prime' is a prime number by calling the is_prime function
+    while check==False: # loop until 'prime' is confirmed to be prime
             check= is_prime(prime)
-    print(generating_keys(prime))
+    #provoking/calling and printing the functions
+    print(generating_keys(prime))  
     print(encryption(message,prime,g,y))
     print(decryption(c1,c2,prime,x))
-    print("Process is done.")
-#provoking/calling the function
-main(message)
+    print("Process is done.") #print a message indicating the process is done
+
+main(message) #provoking/calling the function
 
 
